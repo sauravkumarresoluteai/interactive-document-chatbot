d54e0e97f51a83b8b35f95fe62990fa8d410291d
spynom <saurav24082000@gmail.com>
2025-07-14 16:27:48 +0530

    first commit

---
+++ MyFun.py
@@ -0,0 +1,50 @@
+from langchain.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage,AIMessage
+from langchain_core.messages.utils import convert_to_openai_messages
+import os
+def load_docs(file_path: str) -> list:
+    ext = os.path.splitext(file_path)[1].lower()  # e.g., '.pdf'
+    loader_class = {
+        ".pdf": PyPDFLoader,
+        ".docx": Docx2txtLoader,
+        ".txt": TextLoader
+    }.get(ext)
+    return loader_class(file_path).load()
+
+
+def embedding_model():
+
+    return HuggingFaceEmbeddings(model="all-MiniLM-L6-v2")
+
+
+def chat_model():
+    return ChatGroq(
+        model="gemma2-9b-it",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        # other params...
+    )
+
+
+class Messages:
+    def __init__(self):
+        self.messages = []
+
+    def human_message(self,message:str):
+        self.add_message(HumanMessage(content=message))
+
+    def ai_message(self,message:str):
+        self.add_message(AIMessage(content=message))
+
+    def add_message(self, message: AIMessage|HumanMessage):
+        self.messages.append(message)
+
+    def get_messages(self,*args):
+        return self.messages[-5:]
+
+    def get_openai_format(self):
+        return convert_to_openai_messages(self.messages)
---
+++ main.py
@@ -0,0 +1,86 @@
+from langchain.schema.runnable import RunnableMap, RunnablePassthrough
+from langchain.schema.output_parser import StrOutputParser
+from MyFun import *
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from dotenv import load_dotenv
+import streamlit as st
+import tempfile
+import os
+load_dotenv()
+
+st.title("Interactive Document Chatbot")
+
+
+uploaded_file = st.file_uploader("Upload a pdf", type=["pdf","docx","txt"])
+
+if uploaded_file is not None:
+    file_extension = os.path.splitext(uploaded_file.name)[1]
+    # Save the uploaded file temporarily
+    with tempfile.NamedTemporaryFile(delete=False, suffix=file_extension) as temp_file:
+        temp_file.write(uploaded_file.read())
+        temp_file_path = temp_file.name
+
+    st.session_state.vectorstore = InMemoryVectorStore(embedding_model())
+    docs = load_docs(file_path = temp_file_path)
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    docs = text_splitter.split_documents(docs)
+
+    st.session_state.vectorstore.add_documents(docs)
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = Messages()
+
+
+llm = chat_model()
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """You are an expert assistant for question-answering tasks.
+        Use ONLY the following retrieved context to answer the question.
+        If you don't know the answer from the provided context, just say that you don't know.
+        Your answer should be concise and helpful.
+        context:
+        {context}."""),
+    MessagesPlaceholder("history"),
+    ("human", "{question}")
+])
+
+
+
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages.get_openai_format():
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+if "vectorstore" in st.session_state:
+    question = st.chat_input("Say something")
+    if question:
+
+
+        chain = ( prompt
+                | llm
+                | StrOutputParser()
+        )
+
+        ai_response = chain.invoke({"question": question,"history": st.session_state.messages.get_messages(),"context":"\n\n".join(
+                        doc.page_content for doc in
+                        st.session_state.vectorstore.as_retriever(k=3).invoke(question)
+                    )})
+        st.session_state.messages.human_message(question)
+        st.session_state.messages.ai_message(ai_response)
+        with st.chat_message("user"):
+            st.markdown(question)
+
+        with st.chat_message("assistant"):
+            st.markdown(ai_response)
+            with st.expander("View Sources"):
+                docs = st.session_state.vectorstore.as_retriever(k=3).invoke(question)
+
+                for doc in docs:
+                    st.markdown(doc.page_content.strip())
+                    st.markdown("---")
+
+
